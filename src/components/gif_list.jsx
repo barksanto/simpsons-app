4f3c302a55556227e12bc622ad302a888fa6df5e
Barkley Santo <barkley.santo@gmail.com>
2021-01-11 15:48:43 -0600

    renderList method added, so its called inside of the gif-list div - just seperating concerns

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Gif from './gif'
 
 class GifList extends Component {
-  render() {
+  renderList = () => {
     return this.props.gifs.map(gif => {
       return <Gif id={gif.id} key={gif.id} />
-    });
+    })
+  }
+
+  render() {
+    return (
+      <div className="gif-list">
+        { this.renderList()}
+      </div>
+    );
   }
 }
 
